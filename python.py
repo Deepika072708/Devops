78f8bca373384fde12d5277c044d150303f5fdb2
Deepika072708 <kudumdeepu07@gmail.com>
2026-09-01 11:54:08 +0530

    Implement Jenkins pipeline for Python project
    Added a Jenkins pipeline to clone a repository and run a Python program.

--- python.py
+++ python.py
@@ -1,2 +1,25 @@
-name = input("Enter your name: ")
-print("Hello", name)
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Clone Repository') {
+            steps {
+                git branch: 'main',
+                    url: 'https://github.com/Deepika072708/Devops.git'
+            }
+        }
+
+        stage('Check Python') {
+            steps {
+                sh 'python3 --version'
+            }
+        }
+
+        stage('Run Python Program') {
+            steps {
+                sh 'python3 program.py'
+            }
+        }
+    }
+}
